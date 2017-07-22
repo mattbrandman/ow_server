574ce0b35f2964e0209bd426c651eed72a1a3c78
farid00 <brandman.m@husky.neu.edu>
2017-07-22 17:54:27 -0400

    m

--- app.js
+++ app.js
@@ -16,11 +16,6 @@ var User = require('./models/user');
 var users = require('./routes/users');
 
 var app = express();
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 var io = socket_io();
 app.io = io;
